81a12fcc4d2eef9019fab6e9013be36c30b77fcb
Jan Tattermusch <jtattermusch@users.noreply.github.com>
2018-06-06 08:13:54 +0200

    Update run_bisect_step.sh

--- run_bisect_step.sh
+++ run_bisect_step.sh
@@ -25,7 +25,7 @@ make clean  # sometimes necessary to prevent build breakages
 
 #python tools/run_tests/run_tests.py -l c++ -c asan --build_only  || exit 125
 
-python tools/run_tests/run_tests.py -l c++ -c asan --use_docker -t -r 'ClientCancelsRequestStream' --force_use_pollers epollex -n 10000 -a 40 -j 40 --quiet_success -x "tests_${CURRENT_COMMIT}/sponge_log.xml"
+python tools/run_tests/run_tests.py -l c++ -c asan --use_docker -t -r 'ClientCancelsRequestStream' --force_use_pollers epollex -n 5000 -a 40 -j 40 --quiet_success -x "tests_${CURRENT_COMMIT}/sponge_log.xml"
 
 #python tools/run_tests/run_tests.py -l c -c opt --use_docker -t -r 'httpcli_test' -n 2000 -a 40 -j 30
 
