f9c6ad24fbbb1d8ed5d2686c0f9e52cc06358487
Jan Tattermusch <jtattermusch@users.noreply.github.com>
2018-11-07 15:12:24 +0100

    Update run_bisect_step.sh

--- run_bisect_step.sh
+++ run_bisect_step.sh
@@ -45,8 +45,7 @@ make clean  # sometimes necessary to prevent build breakages
 
 #python tools/run_tests/run_tests.py -l c++ -c dbg -t -r 'grpclb_end2end_test.*SingleBalancerTest.SecureNaming' -n 2000 -a 4 -j 10
 
-python tools/run_tests/run_tests.py -l c -c tsan --compiler clang7.0 --use_docker -t -j 20 --quiet_success -x "${REPORT_NAME}" -r handshake_verify_peer_options --force_use_pollers epoll1 -n 100
+#python tools/run_tests/run_tests.py -l c -c tsan --compiler clang7.0 --use_docker -t -j 20 --quiet_success -x "${REPORT_NAME}" -r handshake_verify_peer_options --force_use_pollers epoll1 -n 100
 
-# TODO: determine the right number of iterations
-# TODO: determine the right regex to run
+python tools/run_tests/run_tests.py -l c++ -c opt -t -r '' -n 5 -j 2 -x "${REPORT_NAME}" -r 'client_channel_stress_test'
 
